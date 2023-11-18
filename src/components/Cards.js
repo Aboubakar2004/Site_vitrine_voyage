5368997bb24745fc14e783686a046a40d9966531
Aboubakar Bamba <aboubakarbamba716@gmail.com>
2023-11-18 01:21:56 +0100

    Ajout du Site en Front end

---
+++ src/components/Cards.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "./Cards.css";
+import CardItem from "./CardItem";
+import Image9 from "./images/img-9.jpg";
+import Image2 from "./images/img-2.jpg";
+import Image3 from "./images/img-3.jpg";
+import Image4 from "./images/img-4.jpg";
+import Image8 from "./images/img-8.jpg";
+
+function Cards() {
+  return (
+    <div className="cards">
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem
+              src={Image9}
+              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label="Adventure"
+              path="/services"
+              alt="Alt image 9"
+            />
+            <CardItem
+              src={Image2}
+              text="Travel through the Islands of Bali in a Private Cruise"
+              label="Luxury"
+              path="/services"
+              alt="Alt image 2"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src={Image3}
+              text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
+              label="Mystery"
+              path="/services"
+              alt="Alt image 3"
+            />
+            <CardItem
+              src={Image4}
+              text="Experience Football on Top of the Himilayan Mountains"
+              label="Adventure"
+              path="/products"
+              alt="Alt image 4"
+            />
+            <CardItem
+              src={Image8}
+              text="Ride through the Sahara Desert on a guided camel tour"
+              label="Adrenaline"
+              path="/sign-up"
+              alt="Alt image 8"
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
